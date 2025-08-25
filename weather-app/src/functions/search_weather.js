1c2cd6fac4211534a411c934de939747b923ad71
Nicholas B <brunernick11@gmail.com>
2025-08-25 11:26:54 -0500

    Basic axios GET request for API with example London

--- weather-app/src/functions/search_weather.js
+++ weather-app/src/functions/search_weather.js
@@ -1,14 +1,14 @@
+import axios from 'axios';
+
+
 export default function search_weather() {
-    console.log(process.env.REACT_APP_API_KEY);
-    /*
-    const axios = require('axios');
+    const api_key = process.env.REACT_APP_API_KEY
     // Making a GET request
-    axios.get('http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={API key}')
+    axios.get('http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=' + api_key)
         .then(response => {
             console.log(response.data); 
         })
         .catch(error => {
             console.error('Error fetching data:', error);
         });
-        */
 }
